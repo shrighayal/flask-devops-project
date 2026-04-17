3cdc983026d16bbba3684f1cb074cd80dd758374
Ubuntu <ubuntu@ip-172-31-38-101.ap-south-1.compute.internal>
2026-04-17 09:07:59 +0000

    2-tier flask mysql nginx app

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 
+def get_db_connection():
+    return pymysql.connect(
+        host='db',
+        user='admin',
+        password='admin123',
+        database='devops_db'
+    )
+
 @app.route('/')
 def home():
-    return "DevOps Flask App Running 🚀"
+    return "Flask + MySQL DevOps App 🚀"
+
+@app.route('/data')
+def data():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50))")
+    cursor.execute("INSERT INTO users (name) VALUES ('Shrikant')")
+    conn.commit()
+
+    cursor.execute("SELECT * FROM users")
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return str(data)
 
 @app.route('/health')
 def health():
